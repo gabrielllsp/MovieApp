2343c34184816d6206354e80990d61bc3e94b40f
gabri <gabriel.amoreira@outlook.com>
2023-12-13 12:20:11 -0300

    Criando grafico de autenticação.

---
+++ app/src/main/java/com/gabrielalmeida/movieapp/presenter/auth/activity/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.gabrielalmeida.movieapp.presenter.auth.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gabrielalmeida.movieapp.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
