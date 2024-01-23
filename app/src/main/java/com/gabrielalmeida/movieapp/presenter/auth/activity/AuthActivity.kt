8178aef2ed56821479c07bda98af192c64fed3c8
gabri <gabriel.amoreira@outlook.com>
2024-01-23 11:20:14 -0300

    configuring status bar and limiting the result of the request.

--- app/src/main/java/com/gabrielalmeida/movieapp/presenter/auth/activity/AuthActivity.kt
+++ app/src/main/java/com/gabrielalmeida/movieapp/presenter/auth/activity/AuthActivity.kt
@@ -2,13 +2,45 @@ package com.gabrielalmeida.movieapp.presenter.auth.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ReportFragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.gabrielalmeida.movieapp.R
+import com.gabrielalmeida.movieapp.databinding.ActivityAuthBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AuthActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var binding: ActivityAuthBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setStatusBarTranslucent()
+        initNavigation()
+    }
+
+    private fun initNavigation(){
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener{_, destination, _ ->
+            if (destination.id !=R.id.onboardingFragment){
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            }
+        }
+    }
+
+    private fun setStatusBarTranslucent(){
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
     }
 }
