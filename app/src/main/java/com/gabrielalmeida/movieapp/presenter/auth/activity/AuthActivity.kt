8f76274a86471a3442a65c6a7d291dcc2d286e11
gabri <gabriel.amoreira@outlook.com>
2024-01-23 12:13:04 -0300

    checking logged in user.

--- app/src/main/java/com/gabrielalmeida/movieapp/presenter/auth/activity/AuthActivity.kt
+++ app/src/main/java/com/gabrielalmeida/movieapp/presenter/auth/activity/AuthActivity.kt
@@ -1,13 +1,15 @@
 package com.gabrielalmeida.movieapp.presenter.auth.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import androidx.lifecycle.ReportFragment
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.gabrielalmeida.movieapp.R
 import com.gabrielalmeida.movieapp.databinding.ActivityAuthBinding
+import com.gabrielalmeida.movieapp.presenter.main.activity.MainActivity
+import com.gabrielalmeida.movieapp.util.FirebaseHelper
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -24,23 +26,32 @@ class AuthActivity : AppCompatActivity() {
 
         setStatusBarTranslucent()
         initNavigation()
+        isAuthenticated()
     }
 
-    private fun initNavigation(){
-        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+    private fun initNavigation() {
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
-        navController.addOnDestinationChangedListener{_, destination, _ ->
-            if (destination.id !=R.id.onboardingFragment){
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id != R.id.onboardingFragment) {
                 window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
             }
         }
     }
 
-    private fun setStatusBarTranslucent(){
+    private fun setStatusBarTranslucent() {
         window.decorView.systemUiVisibility =
             View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
                     View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                     View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+    }
+
+    private fun isAuthenticated() {
+        if (FirebaseHelper.isAuthenticated()) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 }
