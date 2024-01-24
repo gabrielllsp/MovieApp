ecc00fd25ed48ed7799c95b8fbbf9fcb42d2c20a
gabri <gabriel.amoreira@outlook.com>
2024-01-24 11:03:37 -0300

    genre listing screen.

---
+++ app/src/main/java/com/gabrielalmeida/movieapp/presenter/main/moviegenre/MovieGenreViewModel.kt
@@ -0,0 +1,40 @@
+package com.gabrielalmeida.movieapp.presenter.main.moviegenre
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.gabrielalmeida.movieapp.BuildConfig
+import com.gabrielalmeida.movieapp.domain.usecase.movie.GetMoviesByGenreUseCase
+import com.gabrielalmeida.movieapp.util.Constants
+import com.gabrielalmeida.movieapp.util.StateView
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import retrofit2.HttpException
+import javax.inject.Inject
+
+@HiltViewModel
+class MovieGenreViewModel @Inject constructor(
+    private val getMoviesByGenreUseCase: GetMoviesByGenreUseCase
+): ViewModel() {
+
+    fun getMoviesByGenre(genreId: Int?) = liveData(Dispatchers.IO) {
+        try {
+            emit(StateView.Loading())
+
+            val movies = getMoviesByGenreUseCase.invoke(
+                apiKey = BuildConfig.API_KEY,
+                language = Constants.Movie.LANGUAGE,
+                genreId = genreId
+
+            )
+
+            emit(StateView.Success(movies))
+
+        } catch (e: HttpException) {
+            e.printStackTrace()
+            emit(StateView.Error(message = e.message))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            emit(StateView.Error(message = e.message))
+        }
+    }
+}
