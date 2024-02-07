6f71a0582865d5521d073f2ce244dfe62af9f597
gabri <gabriel.amoreira@outlook.com>
2024-02-07 10:43:07 -0300

    setting up similar movie browsing.

--- app/src/main/java/com/gabrielalmeida/movieapp/presenter/main/movie_details/SimilarFragment.kt
+++ app/src/main/java/com/gabrielalmeida/movieapp/presenter/main/movie_details/SimilarFragment.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
+import com.gabrielalmeida.movieapp.MainGraphDirections
 import com.gabrielalmeida.movieapp.R
 import com.gabrielalmeida.movieapp.databinding.FragmentSimilarBinding
 import com.gabrielalmeida.movieapp.presenter.main.bottombar.home.adapter.MovieAdapter
@@ -53,15 +55,19 @@ class SimilarFragment : Fragment() {
         movieAdapter = MovieAdapter(
             context = requireContext(),
             layoutInflater = R.layout.movie_genre_item,
-            movieClickListener = {
-
+            movieClickListener = {movieId ->
+                movieId?.let {
+                    val action = MainGraphDirections
+                        .actionGlobalMovieDetailsFragment(movieId)
+                    findNavController().navigate(action)
+                }
             }
         )
         val mLayoutManager =  GridLayoutManager(requireContext(), 2)
 
         with(binding.recyclerMovies){
             layoutManager = mLayoutManager
-//            setHasFixedSize(true)
+            setHasFixedSize(true)
             adapter = movieAdapter
         }
     }
